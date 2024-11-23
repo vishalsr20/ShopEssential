7cadce8a5c114bbb724254d37ade2bada29300ee
Vishal Subhash Rathod <154747312+Rathod-vishal-20@users.noreply.github.com>
2024-11-23 13:58:15 +0530

    Added fronted link  index.js

--- ShopBackend/index.js
+++ ShopBackend/index.js
@@ -13,7 +13,7 @@ app.use(
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://shop-essential-sxmu.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
