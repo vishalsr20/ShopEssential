2a7515a39c24998f27aab9489586c81f63461efe
Vishal Subhash Rathod <154747312+Rathod-vishal-20@users.noreply.github.com>
2024-11-23 12:53:15 +0530

    comma index.js

--- ShopBackend/index.js
+++ ShopBackend/index.js
@@ -13,7 +13,7 @@ app.use(
 
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "http://localhost:5173",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
